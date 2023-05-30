1e0d3bb60f0889178c6e0bdd5fe4ddf10fc14dbd
JaredJose <jjose@ucsd.edu>
2023-05-30 10:18:16 -0700

    Moved around files within main.js and began unit testing

--- js/main.js
+++ js/main.js
@@ -1,82 +1,90 @@
 window.addEventListener('DOMContentLoaded', init)
 
-function init () {
-    const NUM_CARDS = 5
-    const PATH = '../assets/card_package/fortunes.json'
-    let fiveChosenCards = []
-
-    /**
-     * This class is to represent a tarot card object. Each card will a name, 5
-     * associated fortunes, and an image. 
-     */
-    class Card {
-        constructor (name, fortunes, img) {
-            this.name = name
-            this.fortunes = fortunes
-            this.img = img
-        }
-
-        /**
-         * This method is a getter for the Card's fortunes.
-         * @returns an array of 5 fortunes
-         */
-        getFortunes () {
-            return this.fortunes
-        }
-
-        /**
-         * This method is a getter for the Card's image.
-         * @returns the name of the Card's image
-         */
-        getImg () {
-            return this.img
-        }
+/**
+ * This class is to represent a tarot card object. Each card will a name, 5
+ * associated fortunes, and an image. 
+ */
+class Card {
+    constructor (name, fortunes, img) {
+        this.name = name;
+        this.fortunes = fortunes;
+        this.img = img;
     }
 
     /**
-     * This class is to represent the chosen deck of Tarot card. The deck will
-     * have an array which holds Card objects.
+     * This method is a getter for the Card's assigned name.
+     * @returns the name of the Card
      */
-    class Deck {
-        constructor (path) {
-            this.cards = []
-        }
+    getName() {
+        return this.name;
+    }
+    /**
+     * This method is a getter for the Card's fortunes.
+     * @returns an array of 5 fortunes
+     */
+    getFortunes () {
+        return this.fortunes;
+    }
 
-        /**
-         * This method randomly selects 5 cards out of the Deck. This simulates
-         * the user being randomly dealt 5 tarot cards.
-         * @param num Number of cards to be dealt out
-         * @returns An array of the selected cards
-         */
-        draw (num) {
-            const returnCards = []
-            for (let i = 0; i < num; i++) {
-                let tempCard = this.cards[Math.floor(Math.random() * this.cards.length)]
-                while (returnCards.includes(tempCard)) {
-                    tempCard = this.cards[Math.floor(Math.random() * this.cards.length)]
-                }
-                returnCards.push(tempCard)
+    /**
+     * This method is a getter for the Card's image.
+     * @returns the path/name of the Card's image
+     */
+    getImg () {
+        return this.img;
+    }
+}
+
+/**
+ * This class is to represent the chosen deck of Tarot card. The deck will
+ * have an array which holds Card objects.
+ */
+class Deck {
+    constructor (path) {
+        this.path = path;
+        this.cards = []
+    }
+
+    /**
+     * This method randomly selects a number of cards out of the Deck. This simulates
+     * the user being randomly dealt a number of  tarot cards.
+     * @param num Number of cards to be dealt out
+     * @returns An array of the selected cards
+     */
+    draw (num) {
+        const returnCards = []
+        for (let i = 0; i < num; i++) {
+            let tempCard = this.cards[Math.floor(Math.random() * this.cards.length)]
+            while (returnCards.includes(tempCard)) {
+                tempCard = this.cards[Math.floor(Math.random() * this.cards.length)]
             }
-            return returnCards
+            returnCards.push(tempCard)
         }
+        return returnCards
     }
 
-    /**
+        /**
      * This function fills the Deck with the proper cards according to which Deck
      * the user has chosen.
      * @param path The file path to the JSON file containing deck information
      * @param deckObj The Deck object that will be filled with cards
      * @returns N/A
      */
-    async function generateDeck (path, deckObj) {
-        const response = await fetch(PATH)
-        const data = await response.json()
-
-        for (let i = 0; i < data.deck.card.length; i++) {
-            const tempCard = new Card(data.deck.card[i].name, data.deck.card[i].fortunes, data.deck.card[i].img)
-            deckObj.cards.push(tempCard)
+        async generateDeck () {
+            const response = await fetch(this.path)
+            const data = await response.json()
+    
+            for (let i = 0; i < data.deck.card.length; i++) {
+                const tempCard = new Card(data.deck.card[i].name, data.deck.card[i].fortunes, data.deck.card[i].img)
+                this.cards.push(tempCard)
+            }
         }
-    }
+}
+
+function init () {
+    const NUM_CARDS = 5
+    const PATH = '../assets/card_package/fortunes.json'
+    let fiveChosenCards = []
 
     /**
      * This function handles everything that happens after the Deck has been
@@ -85,7 +93,7 @@ function init () {
     async function main () {
         const myDeck = new Deck(PATH)
 
-        await generateDeck(PATH, myDeck)
+        await myDeck.generateDeck();
         const selectedCards = myDeck.draw(NUM_CARDS)
         fiveChosenCards = selectedCards
 
@@ -204,3 +212,5 @@ function init () {
     }
     main()
 }
+
+module.exports = {Card, Deck};
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,1 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const functions = require('../js/main.js');
+
+test('Successful card generation with valid parameters', () => {
+    let validCard = true;
+    let name = 'example name';
+    let fortuneStrings = ['str1','str2','str3','str4','str5'];
+    let img = 'example/path/';
+    const tempCard = new functions.Card(name,fortuneStrings,img);
+    if(name != tempCard.getName()) {validCard = false;}
+    if(fortuneStrings != tempCard.getFortunes()){validCard = false;}
+    if(img != tempCard.getImg()){validCard = false;}
+
+    expect(validCard).toBe(true);
+});
+
