ab5b309a3027d39f32ffbc140198acf6126eb5a8
amtjitro <60487970+amtjitro@users.noreply.github.com>
2023-05-31 19:04:26 -0700

    Resolve semicolon related linter issues

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ class Card {
 class Deck {
     constructor (path) {
         this.path = path;
-        this.cards = []
+        this.cards = [];
     }
 
     /**
@@ -54,13 +54,13 @@ class Deck {
     draw (num) {
         const returnCards = []
         for (let i = 0; i < num; i++) {
-            let tempCard = this.cards[Math.floor(Math.random() * this.cards.length)]
+            let tempCard = this.cards[Math.floor(Math.random() * this.cards.length)];
             while (returnCards.includes(tempCard)) {
-                tempCard = this.cards[Math.floor(Math.random() * this.cards.length)]
+                tempCard = this.cards[Math.floor(Math.random() * this.cards.length)];
             }
-            returnCards.push(tempCard)
+            returnCards.push(tempCard);
         }
-        return returnCards
+        return returnCards;
     }
 
     /**
@@ -71,39 +71,39 @@ class Deck {
      * @returns N/A
      */
     async generateDeck () {
-        const response = await fetch(this.path)
-        const data = await response.json()
+        const response = await fetch(this.path);
+        const data = await response.json();
 
         for (let i = 0; i < data.deck.card.length; i++) {
-            const tempCard = new Card(data.deck.card[i].name, data.deck.card[i].fortunes, data.deck.card[i].img)
-            this.cards.push(tempCard)
+            const tempCard = new Card(data.deck.card[i].name, data.deck.card[i].fortunes, data.deck.card[i].img);
+            this.cards.push(tempCard);
         }
     }
 }
 
 function init () {
-    const NUM_CARDS = 5
-    const PATH = '../assets/card_package/fortunes.json'
-    let fiveChosenCards = []
+    const NUM_CARDS = 5;
+    const PATH = '../assets/card_package/fortunes.json';
+    let fiveChosenCards = [];
 
     /**
      * This function handles everything that happens after the Deck has been
      * chosen and generated.
      */
     async function main () {
-        const myDeck = new Deck(PATH)
+        const myDeck = new Deck(PATH);
 
         await myDeck.generateDeck();
-        const selectedCards = myDeck.draw(NUM_CARDS)
-        fiveChosenCards = selectedCards
+        const selectedCards = myDeck.draw(NUM_CARDS);
+        fiveChosenCards = selectedCards;
 
-        console.log(selectedCards)
-        let numCardsFlipped = 0
-        const flipped = [false, false, false, false, false]
+        console.log(selectedCards);
+        let numCardsFlipped = 0;
+        const flipped = [false, false, false, false, false];
 
-        const cards = document.getElementsByClassName('card')
+        const cards = document.getElementsByClassName('card');
         for (let i = 0; i < cards.length; i++) {
-            cards[i].addEventListener('click', () => flipCard(i))
+            cards[i].addEventListener('click', () => flipCard(i));
         }
 
         /**
@@ -112,19 +112,19 @@ function init () {
          * @returns N/A
          */
         function flipCard (index) {
-            console.log('card clicked')
+            console.log('card clicked');
             if (flipped[index]) {
-                return
+                return;
             }
 
-            const currentCard = document.getElementsByClassName('card')[index]
-            currentCard.style.backgroundImage = `url(../assets/card_package/${selectedCards[index].img})`
-            numCardsFlipped++
-            currentCard.classList.toggle('card-flip')
+            const currentCard = document.getElementsByClassName('card')[index];
+            currentCard.style.backgroundImage = `url(../assets/card_package/${selectedCards[index].img})`;
+            numCardsFlipped++;
+            currentCard.classList.toggle('card-flip');
 
-            flipped[index] = true
+            flipped[index] = true;
             if (numCardsFlipped === 5) {
-                allFlipped()
+                allFlipped();
             }
         }
     }
@@ -135,8 +135,8 @@ function init () {
      * presented.
      */
     function allFlipped () {
-        const button = document.getElementById('disp-fort-butt')
-        button.style.display = 'block'
+        const button = document.getElementById('disp-fort-butt');
+        button.style.display = 'block';
 
         // on click, load the fortunes and display them
         /*
@@ -148,69 +148,69 @@ function init () {
         */
         button.addEventListener('click', () => {
             // clear the cards off the page
-            document.body.innerHTML = ''
+            document.body.innerHTML = '';
 
-            const fortuneList = []
+            const fortuneList = [];
 
             for (let i = 0; i < 5; i++) {
-                const fortunesOfCard = fiveChosenCards[i].getFortunes()
-                console.log(fortunesOfCard)
-                let fortuneText
+                const fortunesOfCard = fiveChosenCards[i].getFortunes();
+                console.log(fortunesOfCard);
+                let fortuneText;
 
                 switch (i) {
                 // dealing with outcome card
                 case 0:
-                    fortuneText = fortunesOfCard[0]
-                    break
+                    fortuneText = fortunesOfCard[0];
+                    break;
 
                 // dealing with current situation card
                 case 1:
-                    fortuneText = fortunesOfCard[1]
-                    break
+                    fortuneText = fortunesOfCard[1];
+                    break;
 
                 // dealing with challenges card
                 case 2:
-                    fortuneText = fortunesOfCard[2]
-                    break
+                    fortuneText = fortunesOfCard[2];
+                    break;
 
                 // dealing with what u can change card
                 case 3:
-                    fortuneText = fortunesOfCard[3]
-                    break
+                    fortuneText = fortunesOfCard[3];
+                    break;
 
                 // dealing with response card
                 case 4:
-                    fortuneText = fortunesOfCard[4]
-                    break
+                    fortuneText = fortunesOfCard[4];
+                    break;
                 }
-                fortuneList.push(fortuneText)
+                fortuneList.push(fortuneText);
             }
 
             for (let i = 0; i < 5; i++) {
-                const p = document.createElement('p')
+                const p = document.createElement('p');
 
                 // Set the inner text of the paragraph to the fortune
-                p.innerText = fortuneList[i]
+                p.innerText = fortuneList[i];
 
                 // Create an img element for the card image
-                const img = document.createElement('img')
+                const img = document.createElement('img');
 
                 // Set the src attribute of the img element to the image URL
-                img.src = '../assets/card_package/' + fiveChosenCards[i].getImg()
+                img.src = '../assets/card_package/' + fiveChosenCards[i].getImg();
 
                 // Create a div to contain the image and the paragraph
-                const div = document.createElement('div')
+                const div = document.createElement('div');
 
                 // Append the image and the paragraph to the div
-                div.appendChild(img)
-                div.appendChild(p)
+                div.appendChild(img);
+                div.appendChild(p);
 
                 // Append the div to the body
-                document.body.appendChild(div)
+                document.body.appendChild(div);
             }
         })
     }
-    main()
+    main();
 }
 
 module.exports = {Card, Deck};
